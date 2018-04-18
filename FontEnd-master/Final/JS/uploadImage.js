6bc3063feb6ad78ec636aa4c4d2b0c58ea5b1ff3
Minato20119 <31340334+Minato20119@users.noreply.github.com>
2018-04-18 16:38:44 +0700

    Add files via upload

---
+++ FontEnd-master/Final/JS/uploadImage.js
@@ -0,0 +1,71 @@
+function uploadFile() {
+    var target = document.querySelector(".showImage");
+    var file = document.querySelector("input[type=file]").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        target.src = reader.result;
+    };
+    // Ajax
+    $.ajax({
+        method: "POST",
+        url: "fileUpload",
+        data: {fileUpload: reader.result}
+    }).done(function(response) {
+        alert("Success: " + response);
+    }).fail(function(xhr, status, errorThrown) {
+        alert("Error: " + xhr.responseText);
+    });
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        target.src = "";
+    }
+}
+
+function uploadFile2() {
+    var target = document.querySelector(".showImage2");
+    var file = document.querySelector("input[type=file]").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        target.src = reader.result;
+    };
+    // Ajax
+    $.ajax({
+        method: "POST",
+        url: "fileUpload",
+        data: {fileUpload: reader.result}
+    }).done(function(response) {
+        alert("Success: " + response);
+    }).fail(function(xhr, status, errorThrown) {
+        alert("Error: " + xhr.responseText);
+    });
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        target.src = "";
+    }
+}
+
+function uploadFile3() {
+    var target = document.querySelector(".showImage3");
+    var file = document.querySelector("input[type=file]").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        target.src = reader.result;
+    };
+    // Ajax
+    $.ajax({
+        method: "POST",
+        url: "fileUpload",
+        data: {fileUpload: reader.result}
+    }).done(function(response) {
+        alert("Success: " + response);
+    }).fail(function(xhr, status, errorThrown) {
+        alert("Error: " + xhr.responseText);
+    });
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        target.src = "";
+    }
+}
